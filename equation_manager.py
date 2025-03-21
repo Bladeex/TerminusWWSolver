5239f1c43a837637ffa60fd1b6fefb02f7044af4
Bladeex <dannybuncar@gmail.com>
2025-03-21 00:05:44 +0000

    Add solution retrieval method to equation_manager class

--- equation_manager.py
+++ equation_manager.py
@@ -1,3 +1,5 @@
+import equations as eq
+
 class equation_manager:
     def __init__(self, x, y, z):
         self.set_vars(x, y, z)
@@ -7,3 +9,6 @@ class equation_manager:
         if y is None: y = self.vars[1]
         if z is None: z = self.vars[2]
         self.vars = [x, y, z]
+
+    def get_solutions(self):
+        return [eq.solve_eq1(self.vars[0]), eq.solve_eq2(self.vars[1], self.vars[2]), eq.solve_eq3(self.vars[0], self.vars[1], self.vars[2])]
